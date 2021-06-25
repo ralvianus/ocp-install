f69ee2d78ae6cf8f83873be98a645289325f654f
Robert <robert@alvianus.net>
2021-06-26 01:17:16 +0800

    update

--- ocp-install.sh
+++ ocp-install.sh
@@ -2,9 +2,13 @@
 
 # Ask for version input
 read -p 'OCP Version: ' ocp_version
+read -p 'vCenter FQDN: ' vcenter
 echo
 echo
 echo ====================================
+
+# Download requirements
+yum install unzip -y
 
 # Download Openshift Software
 echo Start downloading Openshift $ocp_version
@@ -26,4 +30,28 @@ echo
 mv oc kubectl openshift-install /usr/local/bin/
 rm -f *.tar.gz
 echo Extract complete
+echo ====================================
+
+# Generating SSH Private Key
+echo Generating SSH Key
+ssh-keygen -t rsa -b 2048 -N '' -f ~/.ssh/id_rsa
+eval "$(ssh-agent -s)"
+ssh-add ~/.ssh/id_rsa
+echo Generating SSH Key completed
+echo ====================================
+
+# Downloading vCenter Root CA
+echo Downloading root CA from vCenter
+wget https://$vcenter/certs/download.zip --no-check-certificate
+echo Downloading completed
+echo
+echo Extracting the zip file
+unzip download.zip
+echo Extracting completed
+
+# Rename the cert file
+echo Renaming the cert files
+for f in certs/lin/*.0; do
+  mv -- "$f" "${f%.0}.crt";
+done
 echo ====================================
