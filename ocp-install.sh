62d57927d1d5c7a3229eb5fbc5b361301c81b5ed
Robert <robert@alvianus.net>
2021-06-26 00:58:42 +0800

    update

--- ocp-install.sh
+++ ocp-install.sh
@@ -3,7 +3,26 @@
 # Ask for version input
 read -p 'OCP Version: ' ocp_version
 echo
+echo
+echo ====================================
 
 # Download Openshift Software
+echo Start downloading Openshift $ocp_version
 wget https://mirror.openshift.com/pub/openshift-v4/clients/ocp/stable-$ocp_version/openshift-client-linux.tar.gz
 wget https://mirror.openshift.com/pub/openshift-v4/clients/ocp/stable-$ocp_version/openshift-install-linux.tar.gz
+echo Download complete
+echo ====================================
+echo
+
+# Untar and move the client and installer to bin
+echo Extracting openshift-install-linux.tar.gz
+tar xvf openshift-install-linux.tar.gz
+echo
+
+echo Extracting openshift-client-linux.tar.gz
+tar xvf openshift-client-linux.tar.gz
+echo
+
+mv oc kubectl openshift-install /usr/local/bin/
+echo Extract complete
+echo ====================================
